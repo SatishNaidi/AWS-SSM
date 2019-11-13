d9cd5fb026db67247ce6bc539c02364d297c4ad0
Satish Naidi <SatishNaidi@gmail.com>
2019-11-13 09:50:59 +0530

    Tags only for amzon, Linux and Windows

--- EC2/update_ec2_tags.py
+++ EC2/update_ec2_tags.py
@@ -1,6 +1,7 @@
 import boto3
 import botocore
 import sys
+import pprint
 
 def ec2_list_of_instances(ec2_conn_obj, custom_tag_info, tag_name):
     """
@@ -9,7 +10,7 @@ def ec2_list_of_instances(ec2_conn_obj, custom_tag_info, tag_name):
         "windows": "WINDOWS",
         "linux": "LINUX",
         "amzlnx": "AMAZON"
-    } 
+    }
     :param tag_name: Patch Group
     :return:
     """
@@ -22,17 +23,19 @@ def ec2_list_of_instances(ec2_conn_obj, custom_tag_info, tag_name):
                     each_inst_dict = dict()
                     instance_image = instance.get("ImageId")
                     each_inst_dict["InstanceId"] = instance.get("InstanceId")
-                    if instance.get("Platform") == "windows":
-                        tag_value = custom_tag_info.get("windows","NoTagDefinedForWindows")
-                    else:
-                        tag_value = custom_tag_info.get("linux","NoTagDefinedForLinux")
-
                     image_description = ec2_conn_obj.describe_images(
                         Filters=[{'Name': 'image-id', 'Values': [instance_image]}])
-
+                    # pprint.pprint(image_description)
                     if image_description.get("Images"):
-                        if image_description.get("Images")[0].get("ImageOwnerAlias") == "amazon":
-                            tag_value = custom_tag_info.get("amzlnx","NoTagDefinedForAmzLinux")
+                        image_name = image_description.get("Images")[0].get("Name").lower()
+                        if "windows" in image_name:
+                            tag_value = custom_tag_info.get("windows","NoTagDefinedForWindows")
+                        elif "rhel" in image_name:
+                            tag_value = custom_tag_info.get("linux", "NoTagDefinedForLinux")
+                        elif "amzn" in image_name:
+                            tag_value = custom_tag_info.get("amzlnx", "NoTagDefinedForAmzonLinux")
+                        else:
+                            print("No OS type defined"+instance.get("InstanceId"))
 
                     each_inst_dict["to_be_added_tag"] = {tag_name: tag_value}
                     all_tags = {}
@@ -62,7 +65,7 @@ def add_tags(ec2_conn_obj,tag_info):
 
 def lambda_handler(event,context):
     # Make sure to add the regions which you do operate
-    regions = ["us-east-1", "us-east-2"]
+    regions = ["us-west-2", "us-east-2"]
     tag_name = "Patch Group"
     custom_tag_info = {
         "windows": "WINDOWS",
