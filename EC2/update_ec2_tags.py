8f05e776a804fb8e107b73467aa682f8360e6144
Satish Naidi <SatishNaidi@gmail.com>
2019-11-13 10:13:06 +0530

    Final Working

--- EC2/update_ec2_tags.py
+++ EC2/update_ec2_tags.py
@@ -26,6 +26,7 @@ def ec2_list_of_instances(ec2_conn_obj, custom_tag_info, tag_name):
                     image_description = ec2_conn_obj.describe_images(
                         Filters=[{'Name': 'image-id', 'Values': [instance_image]}])
                     # pprint.pprint(image_description)
+                    tag_value = None
                     if image_description.get("Images"):
                         image_name = image_description.get("Images")[0].get("Name").lower()
                         if "windows" in image_name:
@@ -36,8 +37,11 @@ def ec2_list_of_instances(ec2_conn_obj, custom_tag_info, tag_name):
                             tag_value = custom_tag_info.get("amzlnx", "NoTagDefinedForAmzonLinux")
                         else:
                             print("No OS type defined"+instance.get("InstanceId"))
+                    if tag_value:
+                        each_inst_dict["to_be_added_tag"] = {tag_name: tag_value}
 
-                    each_inst_dict["to_be_added_tag"] = {tag_name: tag_value}
+
+                        #each_inst_dict["to_be_added_tag"] = {tag_name: tag_value}
                     all_tags = {}
                     for each_tag in instance.get("Tags", {}):
                         all_tags[each_tag["Key"]] = each_tag["Value"]
@@ -52,7 +56,7 @@ def ec2_list_of_instances(ec2_conn_obj, custom_tag_info, tag_name):
 def add_tags(ec2_conn_obj,tag_info):
     tags = []
     for each_item in tag_info["to_be_added_tag"]:
-        item1 = {"Key": each_item, "Value": tag_info["to_be_added_tag"][each_item]}
+        item1 = {"Key": each_item, "Value": tag_info["to_be_added_tag"].get(each_item)}
         tags.append(item1)
     response = ec2_conn_obj.create_tags(
         Resources=[
@@ -65,7 +69,7 @@ def add_tags(ec2_conn_obj,tag_info):
 
 def lambda_handler(event,context):
     # Make sure to add the regions which you do operate
-    regions = ["us-west-2", "us-east-2"]
+    regions = ["us-east-1", "us-east-2"]
     tag_name = "Patch Group"
     custom_tag_info = {
         "windows": "WINDOWS",
@@ -91,7 +95,7 @@ def lambda_handler(event,context):
             if each_item["existing_tags"].get("aws:autoscaling:groupName"):
                 response.append("AutoScaling Instance : "+each_item["InstanceId"])
                 each_item["to_be_added_tag"][tag_name]=each_item["existing_tags"].get(to_be_copied_tag_auto_scaling_group,"RequestorSLID Doesn't exits")
-            if each_item["to_be_added_tag"].items() <= each_item["existing_tags"].items():
+            if each_item.get("to_be_added_tag", {}).items() <= each_item.get("existing_tags", {}).items():
                 response.append("Tags Exists for " + each_item["InstanceId"])
             else:
                 response.append("Tags doesn't Exists for " + each_item["InstanceId"])
