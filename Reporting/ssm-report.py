18fbf5c020b35435ed2651190e0e2899e083daf6
Satish Naidi <snaidi@salesforce.com>
2019-12-10 22:13:29 +0530

    All Reports at once

--- Reporting/ssm-report.py
+++ Reporting/ssm-report.py
@@ -4,6 +4,7 @@ import csv
 from datetime import datetime, date
 import json
 import os
+from xlsxwriter.workbook import Workbook
 
 
 def json_serial(obj):
@@ -99,6 +100,28 @@ def gather_instance_patch_info(ssm_client):
         all_instances.extend(page.get("InstanceInformationList", []))
     return all_instances
 
+
+def detailed_instance_patch_report(ssm_client, instance_ids):
+    all_instances_patch_report = []
+    for each_instance in instance_ids:
+        paginator = ssm_client.get_paginator('describe_instance_patches')
+        try:
+            page_iterator = paginator.paginate(InstanceId=each_instance)
+            items = []
+            for each_page in page_iterator:
+                # print(each_page.get("Patches", []))
+                items.extend(each_page.get("Patches", []))
+            items = [dict(item, InstanceId=each_instance) for item in items]
+            instance_patch_report = json.loads(json.dumps(items, default=json_serial))
+            all_instances_patch_report.extend(instance_patch_report)
+        except Exception as outErr:
+            pdb.set_trace()
+            print(outErr)
+            pass
+        # Adding Instance ID to each element
+    return all_instances_patch_report
+
+
 def filter_needed_fields(input_dict, filter_keys):
     final_out = []
     for each_instance in input_dict:
@@ -146,6 +169,75 @@ def write_to_csv(filename, list_of_dict):
     return filename
 
 
+def convert_csv_to_xlsx(out_file_name, csv_list):
+    """
+    Converts all given CSV List of files to EXCEL
+    """
+    if __name__ != "__main__":
+        out_file_name = "/tmp/"+out_file_name
+    try:
+        workbook = Workbook(out_file_name)
+        for each_csv_file in csv_list:
+            sheet_name = os.path.splitext(os.path.basename(each_csv_file))[0]
+            worksheet = workbook.add_worksheet(sheet_name)
+            with open(each_csv_file, 'rt', encoding='utf8') as f:
+                reader = csv.reader(f)
+                for r, row in enumerate(reader):
+                    for c, col in enumerate(row):
+                        worksheet.write(r, c, col)
+        workbook.close()
+        return out_file_name
+    except Exception as err:
+        print(err)
+        return "Unable to create .xlsx file"
+
+
+def patch_base_line_names_to_ids(client, patch_baselines):
+    try:
+        paginator = client.get_paginator('describe_patch_baselines')
+        marker = None
+        response_base_lines = {}
+        response_iterator = paginator.paginate(
+            Filters=[
+                {
+                    'Key': 'NAME_PREFIX',
+                    'Values': patch_baselines
+                }
+            ],
+            PaginationConfig={
+                'StartingToken': marker
+            }
+        )
+        for page in response_iterator:
+            for each_item in page["BaselineIdentities"]:
+                base_line_id = each_item["BaselineId"]
+                base_line_name = each_item["BaselineName"]
+                response_base_lines[base_line_id] = base_line_name
+        return response_base_lines
+    except Exception as Err:
+        print(Err)
+        return False
+
+
+def get_effective_patches(client, patch_base_lines):
+    list_of_patches = []
+    for pbid, pbname in patch_base_lines.items():
+        try:
+            patch_baseline_response = client.describe_effective_patches_for_patch_baseline(
+                BaselineId=pbid
+            )
+            json_serialized = json.loads(json.dumps(patch_baseline_response, default=json_serial))
+            for each_patch in json_serialized["EffectivePatches"]:
+                new_item = each_patch["Patch"]
+                new_item.update(each_patch["PatchStatus"])
+                new_item.update({"PBName": pbname, "PBId": pbid})
+                list_of_patches.append(new_item)
+        except Exception as err:
+            print(err, pbname, pbid)
+            pass
+    return list_of_patches
+
+
 def upload_file_s3(client, bucket_name, to_be_upload_filename):
     only_filename = os.path.basename(to_be_upload_filename)
 
@@ -159,31 +251,50 @@ def lambda_handler(event, context):
     """
     Default Handler
     """
+    # Connection Objects
     ec2_client = boto3.client('ec2', region_name="us-east-1")
     ssm_client = boto3.client('ssm', region_name="us-east-1")
+    csvs_list = []
 
+    try:
+        patch_baselines = os.environ['patch_baselines'].split(",")
+    except Exception as err:
+        print("Env Variable 'patch_baselines' doesn't exits")
+        patch_baselines = ["WindowsApprovedPatches", "AmazonLinuxApprovedPatches", "LinuxApprovedPatches"]
+
+    # PatchBaselines Report
+    response_patch_base_lines = patch_base_line_names_to_ids(ssm_client, patch_baselines)
+    list_of_patches = get_effective_patches(ssm_client, response_patch_base_lines)
+    csvs_list.append(write_to_csv("PatchBaseLineReport.csv", list_of_patches))
+
+    # EC2Report
     field_names = ['InstanceId', 'State', 'IamInstanceProfile', 'Tags', 'LaunchTime']
-
     ec2_info = gather_ec2_instance_info(ec2_client)
     required_info = filter_needed_fields(ec2_info, field_names)
-
     required_info_instance_ids = [item["InstanceId"] for item in ec2_info]
 
-    csvs_list = []
+    # Instance Patch State
     instance_patch_state = gather_instance_patch_states(ssm_client, required_info_instance_ids)
     instance_patch_state = {each_item["InstanceId"]:each_item for each_item in instance_patch_state}
 
+    # Instance Patch Info
     instance_patch_info = gather_instance_patch_info(ssm_client)
     instance_patch_info = {each_item["InstanceId"]: each_item for each_item in instance_patch_info}
 
+    # Detailed Instance Patch Report
+    instance_patch_report = detailed_instance_patch_report(ssm_client,required_info_instance_ids)
+    csvs_list.append(write_to_csv("EC2PatchReport.csv", instance_patch_report))
+
+    # Consolidating EC2 Report, Patch State Report and Instance Patch Info
     for each_ec2 in required_info:
         each_ec2.update(instance_patch_state.get(each_ec2["InstanceId"], {}))
         each_ec2.update(instance_patch_info.get(each_ec2["InstanceId"], {}))
 
-    # csvs_list.append(write_to_csv("InstancePatchStates.csv", list(instance_patch_state.values())))
-    csvs_list.append(write_to_csv("ec2_report.csv", required_info))
+    csvs_list.append(write_to_csv("EC2Report.csv", required_info))
     s3_client = boto3.client("s3",region_name="us-east-1")
     bucket_name = 'madhav-ssm-logs'
+    xls_file = convert_csv_to_xlsx("ConsolidatedReport.xlsx", csvs_list)
+    csvs_list.append(xls_file)
     final_response = {}
     for each_file in csvs_list:
         try:
